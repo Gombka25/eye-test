3f4083bcf7cf8eacdeba72bd404372273933849d
Gombka25@gmail.com <l.sypniewski@gmail.com>
2017-07-07 13:02:44 +0200

    v2 - Piotrek

--- eye_game.py
+++ eye_game.py
@@ -2,10 +2,17 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
+from collections import namedtuple
+
+Level = namedtuple("Level", ['name', 'value'])
 
 
 class EyeGamePage:
     url = "http://igame.com/eye-test/"
+
+    JASTRZAB  = Level(name='jastrząb', value=25)
+    ROBOT = Level(name='robot', value=30)
+
 
     def __init__(self, driver: WebDriver):
         self.driver = driver
@@ -20,11 +27,14 @@ class EyeGamePage:
     def get_current_time(self):
         return self.driver.find_element_by_css_selector('.clock').text
 
-    def get_to_robot_level(self):
-        for i in range(30):
+    def get_to_level(self, num_of_clicks):
+        for i in range(num_of_clicks):
             self.click_chosen_one()
-
         WebDriverWait(self.driver, 20).until(EC.invisibility_of_element_located((By.ID, "timeleft")))
+
+    def get_to_robot_level(self):
+        self.get_to_level(self.ROBOT.value)
+
 
     def get_reached_level(self):
         return self.driver.find_element_by_css_selector('.character-title').text
